a7db8de9aeb1ca42d851cda978a55d4830489c81
KushDastane <kushdastane777@gmail.com>
2024-11-09 00:57:28 +0530

    Initial commit of Web AR heart anatomy project

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2104581.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 202301248, 202301233, 202380524, 202380527, 202466652, 202473250, 202483036, 202712225, 202712226, 202712227, 202712232, 202927575, 202957433 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/202473047/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/202473048/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/202473046/1/ammo.js', 'preload' : true},
+];
